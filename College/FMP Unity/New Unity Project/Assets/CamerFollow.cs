5aadf7132a6f130fe44697617f0c7d69acc752bf
lcifty <louis.kift@hotmail.co.uk>
2020-02-24 10:26:09 +0000

    ass

---
+++ College/FMP Unity/New Unity Project/Assets/CamerFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine.Transform;
+
+
+public class CamerFollow : MonoBehaviour
+{
+  public Transform target;
+
+  public float smoothSpeed = 0.125f;
+
+  public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+
+}
