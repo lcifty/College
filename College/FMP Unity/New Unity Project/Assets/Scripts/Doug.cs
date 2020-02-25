32744e4f4e9afbf6c0324c08c874dd2a97a27175
lcifty <louis.kift@hotmail.co.uk>
2020-02-25 16:28:41 +0000

    sss

--- College/FMP Unity/New Unity Project/Assets/Scripts/Doug.cs
+++ College/FMP Unity/New Unity Project/Assets/Scripts/Doug.cs
@@ -10,15 +10,5 @@ public class Doug : MonoBehaviour
         Counter.Score += 1;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
