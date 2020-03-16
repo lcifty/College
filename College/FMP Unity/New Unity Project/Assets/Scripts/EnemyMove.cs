e155281c5c0b079fff8c84513c70be05b5f721f8
lcifty <louis.kift@hotmail.co.uk>
2020-03-16 16:43:06 +0000

    ttttt

--- College/FMP Unity/New Unity Project/Assets/Scripts/EnemyMove.cs
+++ College/FMP Unity/New Unity Project/Assets/Scripts/EnemyMove.cs
@@ -6,6 +6,7 @@ public class EnemyMove : MonoBehaviour
 {
 
     public float speed;
+    public bool turned = true;
     private float waitTime;
     public float startWaitTime;
 
@@ -21,10 +22,21 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePoints();
+    }
+
+    void MovePoints()
+    {
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.1f)
         {
+            if (turned == true)
+            {
+                transform.Rotate(Vector2.up * 180);
+                turned = false;
+            }
+            
             if (waitTime <= 0)
             {
                 randomSpot = Random.Range(0, moveSpots.Length);
@@ -34,6 +46,16 @@ public class EnemyMove : MonoBehaviour
             {
                 waitTime -= Time.deltaTime;
             }
+
+            if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) > 0.1f)
+            {
+                turned = true;
+            }
         }
+    }
+
+    void Turn()
+    {
+
     }
 }
--- College/FMP Unity/New Unity Project/Assets/Scripts/KeyHolder.cs
+++ College/FMP Unity/New Unity Project/Assets/Scripts/KeyHolder.cs
@@ -1,25 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyHolder : MonoBehaviour
 {
+    public event EventHandler OnKeysChanged;
+
     private List<Key.KeyType> keyList;
+    public GameObject keyimage;
+
 
     private void Awake()
     {
         keyList = new List<Key.KeyType>();
     }
 
+    public List<Key.KeyType> GetKeyList()
+    {
+        return keyList;
+    }
+
     public void AddKey(Key.KeyType keyType)
     {
         Debug.Log("Added Key: " + keyType);
         keyList.Add(keyType);
+        OnKeysChanged?.Invoke(this, EventArgs.Empty);
     }
 
     public void RemoveKey(Key.KeyType keyType)
     {
         keyList.Remove(keyType);
+        Debug.Log("Removed Key: " + keyType);
+        OnKeysChanged?.Invoke(this, EventArgs.Empty);
     }
 
     public bool ContainsKey(Key.KeyType keyType)
@@ -29,14 +42,30 @@ public class KeyHolder : MonoBehaviour
 
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         Key key = collider.GetComponent<Key>();
         if (key != null)
         {
             AddKey(key.GetKeyType());
             Destroy(key.gameObject);
+            keyimage.SetActive(true);
         }
+
+        KeyDoor keyDoor = collider.GetComponent<KeyDoor>();
+        if (keyDoor != null)
+        {
+            if (ContainsKey(keyDoor.GetKeyType()))
+            {
+                RemoveKey(keyDoor.GetKeyType());
+                keyDoor.OpenDoor();
+                keyimage.SetActive(false);
+            }
+        }
+
+
+
+
     }
 
 
